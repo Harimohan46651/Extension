286183b883259f2a06e6992fcb9410527ba7a679
Harimohan . <112467241+Harimohan46651@users.noreply.github.com>
2024-07-17 23:19:26 +0530

    Till read Json

--- readJSON.js
+++ readJSON.js
@@ -1,19 +1,3 @@
-// const fs = require('fs');
-
-// // Read the JSON file
-// fs.readFile('./exampleJson.JSON', 'utf8', (err, data) => {
-//     if (err) {
-//         console.error(err);
-//         return;
-//     }
-
-//     // Parse the JSON data
-//      JSON.parse(data);
-
-//     // Access the data
-//     console.log(jsonData);
-// });
-
 const obj = {name: 'Sara', age: 25}
 const json = JSON.stringify(obj)
 console.log(json)
